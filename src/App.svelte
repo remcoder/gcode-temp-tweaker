fadf6651e10b9f240cb21d67a7c5dba0b9db280f
Remco Veldkamp <remcoder@gmail.com>
2021-06-04 22:57:36 +0200

    add inputs for temp and flow rate

--- src/App.svelte
+++ src/App.svelte
@@ -19,6 +19,8 @@
   let volume = 0;
   let minFlow = 0;
   let maxFlow = 0;
+  let minTemp = 200;
+  let maxTemp = 230;
   const cur = { x: 0, y: 0, z: 0, e: 0, f:1 };
   init();
 
@@ -65,13 +67,13 @@
   
   // for now we only look at the feed rate
   function analyseFlow(preview) {
-    console.log(preview.layers);
+    // console.log(preview.layers);
 
     return preview.layers.map( l => analyzeLayer(l.commands) );
   }
 
   function analyzeLayer(commands) {
-    console.log(commands);
+    // console.log(commands);
     let totalE = 0, totalT = 0;
 
     for (const cmd of commands) {
@@ -132,31 +134,50 @@
 <main>
 	<h1>GCode Temp Tweaker</h1>
 	
-  <Dropzone on:drop={handleFilesSelect} />
+  <div class="wrapper">
+  <section> 
+    <h2>Source</h2>
+    <Dropzone on:drop={handleFilesSelect} />
 
-  <div id="gcode-preview"></div>
+    <div id="gcode-preview"></div>
 
-  <div> total time:  {time/60}min</div>
-  <div> total extruded:  {Math.round(extruded)}mm</div>
-  <div> total volume:  {Math.round(volume)}mm^3</div>
-  <div> minimum flow rate {minFlow.toFixed(2)} </div>
-  <div> maximum flow rate {maxFlow.toFixed(2)} </div>
-	<ol>
-	{#each layers as layer}
-		<li>{Math.round(layer.totalE)}mm 
-      {Math.round(layer.totalT)}s 
-      {layer.flow.toFixed(2)}mm/s 
-      {layer.vol.toFixed(3) }mm^3/s  
-      {(100 * layer.flow / maxFlow).toFixed(2) }%
-      </li>
-	{/each}
-	</ol>
+    <div> total time:  {time/60}min</div>
+    <div> total extruded:  {Math.round(extruded)}mm</div>
+    <div> total volume:  {Math.round(volume)}mm^3</div>
+    <div> minimum flow rate {minFlow.toFixed(2)} </div>
+    <div> maximum flow rate {maxFlow.toFixed(2)} </div>
+    <ol>
+    {#each layers as layer}
+      <li>{Math.round(layer.totalE)}mm 
+        {Math.round(layer.totalT)}s 
+        {layer.flow.toFixed(2)}mm/s 
+        {layer.vol.toFixed(3) }mm^3/s  
+        {(100 * layer.flow / maxFlow).toFixed(2) }%
+        </li>
+    {/each}
+    </ol>
+  </section>
+  <section>
+    <h2>target</h2>
+    <div class="column-wrapper">
+      <div class="column">
+        <label for="minFlow">min flow</label><input type="number" name="minFlow" value={minFlow.toFixed(2)} step="0.01" />
+        <label for="maxFlow">max flow</label><input type="number" name="maxFlow" value={maxFlow.toFixed(2)} step="0.01" />
+      </div>
+
+      <div class="column">
+        <label for="minTemp">min temp</label><input type="number" name="minTemp" bind:value={minTemp} />
+        <label for="maxTemp">max temp</label><input type="number" name="maxTemp" bind:value={maxTemp} />
+      </div>
+    </div>
+  </section>
+</div>
 </main>
 
 <style>
 	main {
 		text-align: center;
-		padding: 1em;
+		/* padding: 1em; */
 		max-width: 240px;
 		margin: 0 auto;
 	}
@@ -167,6 +188,19 @@
 		font-size: 4em;
 		font-weight: 100;
 	}
+
+  .wrapper {
+    display: flex;
+  }
+
+  .wrapper section {
+    width: 50vw;
+  }
+
+  .column-wrapper {
+    display: flex;
+    justify-content: space-around;
+  }
 
 	@media (min-width: 640px) {
 		main {
